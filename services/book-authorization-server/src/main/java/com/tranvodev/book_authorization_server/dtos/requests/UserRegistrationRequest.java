a86af1b28ed7969fbf75d76ee0190167293b792c
tranvo-dev <tranvo.developer@gmail.com>
2026-07-19 21:24:14 +0700

    refactor: remove @NonNull and @Min
    @Min applies for numeric constraint, no effect on String. Remove @NonNull to prevent 500 when a field in the record is missing causing the RegistrationController.java:26 partly unreachable

--- services/book-authorization-server/src/main/java/com/tranvodev/book_authorization_server/dtos/requests/UserRegistrationRequest.java
+++ services/book-authorization-server/src/main/java/com/tranvodev/book_authorization_server/dtos/requests/UserRegistrationRequest.java
@@ -1,7 +1,4 @@
 package com.tranvodev.book_authorization_server.dtos.requests;
 
-import jakarta.validation.constraints.Min;
-import lombok.NonNull;
-
 public record UserRegistrationRequest(
-        @NonNull String firstName, @NonNull String lastName, @NonNull String email, @NonNull @Min(8) String password) {}
+        String firstName, String lastName, String email, String password) {}
