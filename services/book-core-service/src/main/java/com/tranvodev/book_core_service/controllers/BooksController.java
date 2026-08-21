9223fe01ef229a05901a849e7b465c42d563f074
tranvo-dev <tranvo.developer@gmail.com>
2026-08-21 16:33:11 +0700

    feat: introduce new download book api
    Signed-off-by: tranvo-dev <tranvo.developer@gmail.com>

--- services/book-core-service/src/main/java/com/tranvodev/book_core_service/controllers/BooksController.java
+++ services/book-core-service/src/main/java/com/tranvodev/book_core_service/controllers/BooksController.java
@@ -3,10 +3,13 @@ package com.tranvodev.book_core_service.controllers;
 import com.tranvodev.book_core_service.api.BooksApi;
 import com.tranvodev.book_core_service.security.CurrentUserProvider;
 import com.tranvodev.book_core_service.services.BooksService;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 public class BooksController implements BooksApi {
@@ -22,5 +25,10 @@ public class BooksController implements BooksApi {
     public ResponseEntity<Void> uploadBook(MultipartFile file) {
         this.booksService.uploadAttachment(currentUserProvider.getAuthenticatedUser(), file);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Override
+    public ResponseEntity<Resource> downloadBook(UUID bookId) {
+        return BooksApi.super.downloadBook(bookId);
     }
 }
