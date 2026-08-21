033a763f9685d441ba040c2c3ca1edff1118af48
tranvo-dev <tranvo.developer@gmail.com>
2026-08-21 14:42:34 +0700

    refactor: current user resolver
    Centralize user authentication context, replace nimbus jwt with spring jwt for better integration with Spring Security and retrieve claims easier without going through claim set.
    
    Signed-off-by: tranvo-dev <tranvo.developer@gmail.com>

--- services/book-core-service/src/main/java/com/tranvodev/book_core_service/controllers/UsersController.java
+++ services/book-core-service/src/main/java/com/tranvodev/book_core_service/controllers/UsersController.java
@@ -1,16 +1,12 @@
 package com.tranvodev.book_core_service.controllers;
 
-import com.nimbusds.jwt.JWT;
-import com.nimbusds.jwt.JWTParser;
 import com.tranvodev.book_core_service.api.UsersApi;
 import com.tranvodev.book_core_service.dto.UserResponse;
-import com.tranvodev.book_core_service.exceptions.UserTokenInvalidException;
+import com.tranvodev.book_core_service.security.CurrentUserProvider;
 import com.tranvodev.book_core_service.services.UserService;
-import java.text.ParseException;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,20 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsersController implements UsersApi {
     private final UserService userService;
+    private final CurrentUserProvider currentUserProvider;
 
     @Override
     public ResponseEntity<UserResponse> getCurrentUser() {
-        Jwt jwt =
-                (Jwt) Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication())
-                        .getPrincipal();
-        return ResponseEntity.ok(userService.resolveCurrentUser(toNimbusJwt(Objects.requireNonNull(jwt))));
-    }
-
-    private JWT toNimbusJwt(Jwt jwt) {
-        try {
-            return JWTParser.parse(jwt.getTokenValue());
-        } catch (ParseException parseException) {
-            throw new UserTokenInvalidException("Given token is invalid");
-        }
+        Jwt jwt = currentUserProvider.getUserJwt();
+        return ResponseEntity.ok(userService.resolveCurrentUser(Objects.requireNonNull(jwt)));
     }
 }
--- services/book-core-service/src/main/java/com/tranvodev/book_core_service/services/UserService.java
+++ services/book-core-service/src/main/java/com/tranvodev/book_core_service/services/UserService.java
@@ -1,8 +1,8 @@
 package com.tranvodev.book_core_service.services;
 
-import com.nimbusds.jwt.JWT;
 import com.tranvodev.book_core_service.dto.UserResponse;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 public interface UserService {
-    UserResponse resolveCurrentUser(JWT jwt);
+    UserResponse resolveCurrentUser(Jwt jwt);
 }
